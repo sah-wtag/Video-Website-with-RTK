f8a11d75dc5404fd615262d412e6aa3b99f394b2
Md Sadman Ahmed <164001931+sah-wtag@users.noreply.github.com>
2026-07-19 18:04:38 +0600

    Setup Routing - react-router-dom

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ http://localhost:9000
 npm install json-server
 ```
 
-# In this commit, We Have Setup React User Interface
+# We Have Setup React User Interface
 
 1. We have created `navbar` folder and added `Navbar`, `Search` component in react
 2. We have created `tags` folder and added `Tags`, `Tag` component in react
@@ -40,3 +40,9 @@ npm install json-server
 10. We added created `list` folder with `RelatedVideoList`, `RelatedVideoListItem` components
 11. We added created `pages` folder with `Home`, `Video` components
 12. In the `pages/Video` component, we are calling the video related components
+
+# In this commit, Setup Routing - react-router-dom
+
+1. Set the react `router` dom
+2. `Footer` and `Navbar` component in `App` compoents `router` level
+3. Use manual linkup with `Link` in the VideoGridItem to hit the URL and reroute to new page
--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,5 +1,3 @@
-import Navbar from "../components/navbar/Navbar";
-import Footer from "../components/Footer";
 import VideoPlayer from "../components/description/Player";
 import VideoDescription from "../components/description/VideoDescription";
 import RelatedVideoList from "../components/list/RelatedVideoList";
@@ -7,22 +5,17 @@ import RelatedVideoList from "../components/list/RelatedVideoList";
 export default function Video() {
   return (
     <>
-      <Navbar />
       <section class="pt-6 pb-20">
         <div class="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
           <div class="grid grid-cols-3 gap-2 lg:gap-8">
             <div class="col-span-full w-full space-y-8 lg:col-span-2">
-              {/* <!-- video player --> */}
               <VideoPlayer />
-              {/* <!-- video description --> */}
               <VideoDescription />
             </div>
-            {/* <!-- related videos --> */}
             <RelatedVideoList />
           </div>
         </div>
       </section>
-      <Footer />
     </>
   );
 }
